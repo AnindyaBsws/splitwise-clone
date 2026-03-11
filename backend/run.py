3e1897fae498ddcabaf43b6c3e27dca99b5fd954
AnEye <shakhar004@gmail.com>
2026-03-11 17:36:50 +0530

    auto run migrations on startup

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,13 @@
 from app import create_app
 from app.extensions.db import db
-from flask_migrate import Migrate
+from flask_migrate import upgrade
 from flask_cors import CORS
 
 app = create_app()
-migrate = Migrate(app, db)
+
+# Run migrations automatically on startup
+with app.app_context():
+    upgrade()
 
 if __name__ == "__main__":
     app.run(debug=True)
